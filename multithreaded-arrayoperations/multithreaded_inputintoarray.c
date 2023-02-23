e9b732f33672a337c3d3894f5f370fc7d589aecb
Andreas Johansson <andreas.johansson1.external@veoneer.com>
2023-02-23 13:18:15 +0100

    Add file to add code for a function which turns input into array.

---
+++ multithreaded-arrayoperations/multithreaded_inputintoarray.c
@@ -0,0 +1,11 @@
+/*
+ * Write a multithreaded function which converts input in argv
+ * into an array.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+const int main(const int argc, char **argv)
+{
