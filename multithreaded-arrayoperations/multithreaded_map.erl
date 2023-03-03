34bbf1c44f249f5cd1bb7b7f05f1f2743f7cc3a9
Andreas Johansson <aj854@outlook.com>
2023-03-03 21:55:19 +0100

    Added thread divider calculation for erlang version of multithreaded map.

--- multithreaded-arrayoperations/multithreaded_map.erl
+++ multithreaded-arrayoperations/multithreaded_map.erl
@@ -5,11 +5,37 @@
 
 map(NumberOfThreads,Function,List) ->
         ParentPid = self(),
-        [receive
-                 {Pid,Result} ->
-                         Result
-         end
-         || Pid <- [spawn(fun() ->
-                                          ParentPid ! {self(),Function(X)}
-                          end)
-                          || X <- List]].
+        lists:flatten([receive
+                               {Pid,Result} ->
+                                       Result
+                       end
+                       || Pid <- [spawn(fun() ->
+                                                        io:fwrite("#~w: ~w~n",[self(),L]),
+                                                        ParentPid ! {self(),lists:map(Function,L)}
+                                        end)
+                                  || L <- [X || X <- group2(NumberOfThreads,List)]]]).
+
+group(N,L) ->
+        group(N,L,[]).
+group(_,[],Acc) ->
+        Acc;
+group(_,[H|[]],Acc) ->
+        Acc ++ [[H]];
+group(N,L,Acc) ->
+        {H,T} = lists:split(N,L),
+        group(N,T,Acc ++ [H]).
+
+group2(N,L) ->
+        Length = length(L),
+        Split = Length div N,
+        Remainder = Length rem N,
+        InnerFun = fun Rec(I,_,Output) when I > N ->
+                                   Output;
+                       Rec(I,Input,Output) when I =< Remainder ->
+                                   {Head,Tail} = lists:split(Split + 1, Input),
+                                   Rec(I + 1,Tail,Output ++ [Head]);
+                       Rec(I,Input,Output) ->
+                                   {Head,Tail} = lists:split(Split, Input),
+                                   Rec(I + 1,Tail,Output ++ [Head])
+                   end,
+        InnerFun(1,L,[]).
