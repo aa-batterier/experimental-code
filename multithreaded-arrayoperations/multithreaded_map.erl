36d07a4829a0703930e407be20b18261260c9da3
Andreas Johansson <aj854@outlook.com>
2023-03-04 10:31:00 +0100

    Reshuffled the code.

--- multithreaded-arrayoperations/multithreaded_map.erl
+++ multithreaded-arrayoperations/multithreaded_map.erl
@@ -1,5 +1,3 @@
-% Does work, but it creates a new thread for each element in the list.
-
 -module(multithreaded_map).
 -compile(export_all).
 
@@ -13,19 +11,9 @@ map(NumberOfThreads,Function,List) ->
                                                         io:fwrite("#~w: ~w~n",[self(),L]),
                                                         ParentPid ! {self(),lists:map(Function,L)}
                                         end)
-                                  || L <- [X || X <- group2(NumberOfThreads,List)]]]).
+                                  || L <- [X || X <- group(NumberOfThreads,List)]]]).
 
 group(N,L) ->
-        group(N,L,[]).
-group(_,[],Acc) ->
-        Acc;
-group(_,[H|[]],Acc) ->
-        Acc ++ [[H]];
-group(N,L,Acc) ->
-        {H,T} = lists:split(N,L),
-        group(N,T,Acc ++ [H]).
-
-group2(N,L) ->
         Length = length(L),
         Split = Length div N,
         Remainder = Length rem N,
@@ -39,3 +27,13 @@ group2(N,L) ->
                                    Rec(I + 1,Tail,Output ++ [Head])
                    end,
         InnerFun(1,L,[]).
+
+group2(N,L) ->
+        group(N,L,[]).
+group2(_,[],Acc) ->
+        Acc;
+group2(_,[H|[]],Acc) ->
+        Acc ++ [[H]];
+group2(N,L,Acc) ->
+        {H,T} = lists:split(N,L),
+        group(N,T,Acc ++ [H]).
